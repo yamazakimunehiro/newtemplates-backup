ae1d04f3bcd1f3be25924a5f736ebca323153c76
go2020h <go2020.hidane.inc@gmail.com>
2025-07-28 17:45:33 +0900

    fix

--- pages/store.js
+++ pages/store.js
@@ -16,23 +16,14 @@ import {useModal} from "@/internal/providers/modal-provider";
 
 // We're creating a Wix client using the createClient function from the Wix SDK.
 const myWixClient = createClient({
-    // We specify the modules we want to use with the client.
-    // In this case, we're using the products, currentCart, and redirects modules.
     modules: {products, currentCart, redirects},
-
-    // We're using the OAuthStrategy for authentication.
-    // This strategy requires a client ID and a set of tokens.
+    siteId: process.env.WIX_SITE_ID, // ✅ これを追加
     auth: OAuthStrategy({
-        // The client ID is a unique identifier for the application.
-        // It's used to authenticate the application with the Wix platform.
         clientId: CLIENT_ID,
-
-        // The tokens are used to authenticate the user.
-        // In this case, we're getting the tokens from a cookie named "session".
-        // If the cookie doesn't exist, we default to null.
         tokens: JSON.parse(Cookies.get("session") || null),
     }),
 });
+
 
 export default function Store() {
     // State variables for product list and cart
