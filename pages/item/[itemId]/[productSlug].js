2411a004a820fcecb6ebb9f4111429329d614798
go2020h <go2020.hidane.inc@gmail.com>
2025-07-30 11:13:49 +0900

    fix

--- pages/item/[itemId]/[productSlug].js
+++ pages/item/[itemId]/[productSlug].js
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { myWixClient } from "../../../lib/wixClient"; // ✅ 相対パスに修正
+import { myWixClient } from "../../../src/lib/wixClient";
 
 export default function ProductDetailPage() {
   const { query } = useRouter();
